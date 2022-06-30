827717455df811f9dd01e8d53f1efb7b6ca91e7b
kangmuni <kangmoonee@gmail.com>
2022-06-30 22:04:53 +0900

    Edit keyframe for animaition

--- src/components/left.jsx
+++ src/components/left.jsx
@@ -6,6 +6,42 @@ const smoothAppear = keyframes`
   opacity: 0;
   transform: translateY(10%)
 }
+10% {
+  opacity: 0.1;
+  transform: translateY(9%)
+}
+20% {
+  opacity: 0.2;
+  transform: translateY(8%)
+}
+30% {
+  opacity: 0.3;
+  transform: translateY(7%)
+}
+40% {
+  opacity: 0.4;
+  transform: translateY(6%)
+}
+50% {
+  opacity: 0.5;
+  transform: translateY(5%)
+}
+60% {
+  opacity: 0.6;
+  transform: translateY(4%)
+}
+70% {
+  opacity: 0.7;
+  transform: translateY(3%)
+}
+80% {
+  opacity: 0.8;
+  transform: translateY(2%)
+}
+90% {
+  opacity: 0.9;
+  transform: translateY(1%)
+}
 100% {
   opacity: 1;
   transform: translateY(0);
